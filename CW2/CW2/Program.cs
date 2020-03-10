b545de89a4183cf7c0db2d596f08710ace510ff0
Taras Kostiuk <taraskostyuk5@gmail.com>
2020-03-10 20:38:41 +0100

    Mainly works

--- CW2/CW2/Program.cs
+++ CW2/CW2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CW2
 {
@@ -6,7 +7,39 @@ namespace CW2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // if(args[0] == null )
+            //{
+            //  args[0] = "data.csv";
+            //args[1] = "result.xml";
+            //args[2] = "xml";
+            //}
+            string path = @"Data\dane.csv";
+            Console.WriteLine("Hello World");
+
+            var plik = new FileInfo(path);
+            using (var stream = new StreamReader(plik.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] info = line.Split(',');
+                    Console.WriteLine(line);
+                    
+                }
+            }
+
+            //if (File.Exists(args[1]))
+            //{
+            //    FileStream create = File.Open(args[1],FileMode.Create);
+            //}
+
+
+           // if(!Directory.Exists(args[0]))
+
+
+
+
         }
     }
 }
